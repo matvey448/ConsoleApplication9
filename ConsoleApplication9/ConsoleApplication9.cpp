61014386dde2b1f3a432ca4c8a3efff226629d67
mikha <mikha@DESKTOP-VP3U0QJ>
2024-11-06 01:14:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+// Функция выделяет память под переменную нужного типа и возвращает указатель
+int* allocateMemory() {
+    int* ptr = new int(42);  
+    return ptr;
+}
+
+int main() {
+    const int iterations = 10000000; 
+    int* pointers[iterations];      
+
+    
+    for (int i = 0; i < iterations; ++i) {
+        pointers[i] = allocateMemory();
+    }
+
+    
+    std::cout << "enter";
+    std::cin.get();
+
+    
+    for (int i = 0; i < iterations; ++i) {
+        delete pointers[i]; 
+    }
+
+    return 0;
+}
